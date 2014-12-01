6052a08e655299ddb27510d431bc82dfd02ee9cb
AHandless <disy2007-sd33@yahoo.com>
2014-12-02 00:03:20 +1000

    Revert "Rewrite LINQ queries"
    This reverts commit 51ccc8e61cd988ae0d2445814d6b2aacea254c5f.

--- WaveGenerator/Program.cs
+++ WaveGenerator/Program.cs
@@ -9,24 +9,11 @@ namespace WaveGenerator
     class Program
     {
         static void Main()
-        {
-            Stopwatch sw = new Stopwatch();
+        {          
             FileStream file = new FileStream(@"test.wav", FileMode.Create);
-            WaveFile wavefile = new WaveFile(44100, BitDepth.Bit16, 1, file);          
+            WaveFile wavefile = new WaveFile(10000, BitDepth.Bit16, 1, file);          
             SoundGenerator sg = new SoundGenerator(wavefile);
-            sw.Start();
-            sg.AddComplexTone(duration: 1000 * 60 * 30,
-                              startPhases: new double[3],
-                              amplitude: 0.5,
-                              fade: true,
-                              frequencies: new double[] { 400, 500, 600 });
-            //sg.AddSimpleTone(frequency:  400,
-            //                 duration:   1000 * 60 * 30,
-            //                 startPhase: 0,
-            //                 amplitude:  1,
-            //                 fade:       false);
-            sw.Stop();
-            Console.WriteLine(sw.Elapsed);
+            sg.AddComplexTone(1000, new double[3], 1, false, 400, 500, 600);
             sg.Save(); 
             Console.ReadKey();           
         }
